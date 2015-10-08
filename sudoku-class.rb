7ce1f955312edd4e0c267ff3e42750aecbb1f1a5
kromitj <kroskam8457@hotmail.com>
2015-10-07 23:01:05 -0500

    rm colorize on board, added game_complete? function, and now player can change mind on numbers they've played

--- sudoku-class.rb
+++ sudoku-class.rb
@@ -59,12 +59,12 @@ class Sudoku
 				columns << current_column
 			end 
 		end
-		return columns.to_s
+		return columns
 	end
 
 	def display_board
 		@num_locations.each do |i|
-			@board[i[0]][i[1]] = @board[i[0]][i[1]].to_s.colorize(:light_green) 
+			@board[i[0]][i[1]] = @board[i[0]][i[1]] 
 		end	
 		formated = add_row_seps(add_column_seps(get_rows))
 		formated.each { |row| puts "               " + row.join(" ")}
@@ -93,9 +93,14 @@ class Sudoku
 	end
 
 	def move_valid?
-		value_at_move = @board[@user_input[0].to_i][@user_input[1].to_i]	
-		return true if @board[@user_input[0].to_i][@user_input[1].to_i] == "*"
-		return false
+		return false if @user_input[0].to_i.between?(0, 8) == false
+		return false if @user_input[1].to_i.between?(0, 8) == false
+		return false if @user_input[2].to_i.between?(0, 8) == false
+		return false if @num_locations.include?([@user_input[0].to_i, @user_input[1].to_i])
+		# value_at_move = @board[@user_input[0].to_i][@user_input[1].to_i]	
+		# return true if @board[@user_input[0].to_i][@user_input[1].to_i] == "*"
+		# return false
+		return true
 	end
 
 	def set_move
@@ -104,29 +109,33 @@ class Sudoku
 		end
 	end
 
+	def all_nums?(arr)
+		return false if arr.include?("*")
+		arr_sort = arr.sort
+		return true if arr_sort == Array(1..9)
+	end
+
+	def check_rows(rows)
+		rows.each { |row| return false if all_nums?(row) == false}
+		return true
+	end
+
+	def check_columns(columns)
+		columns.each { |column| return false if all_nums?(column) == false}
+		return true
+	end
+
+	def check_grids(grids)
+		grids.each { |grid| return false if all_nums?(grid) == false}
+		return true
+	end
+
 	def game_complete?
 		rows = get_rows
-		# puts rows.to_s
 		columns = get_columns
-		rows.each do |row|
-			# puts "row complete #{row.all? {|i| i.is_a?(Integer) }}" 
-			if (row.all? {|i| i.is_a?(Integer) }) && (Array(1..9) == row.sort)
-				return true
-			else return false
-			end
-		end
-		columns.each do |column| 
-			if (column.all? {|i| i.is_a?(Integer) }) && (Array(1..9) == column.sort)
-				return true
-			else return false
-			end
-		end
-		@board.each do |grid| 
-			if (grid.all? {|i| i.is_a?(Integer) }) && (Array(1..9) == grid.sort)
-				return true
-			else return false
-			end
-		end
+		grids = @board
+		return true if (check_grids(grids)) && (check_columns(columns)) && (check_grids(grids))
+		return false		
 	end
 
 	def show_title
